d2a093c0ead1e535348cbdbad7dfd4260fcf0c2f
AbuJubair7 <jubaircode7@gmail.com>
2026-05-15 10:10:01 +0600

    feat(experiences): module created

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { SkillsModule } from './skills/skills.module';
+import { ExperiencesModule } from './experiences/experiences.module';
 import 'dotenv/config';
 
 @Module({
@@ -13,6 +14,7 @@ import 'dotenv/config';
     UsersModule,
     AuthModule,
     SkillsModule,
+    ExperiencesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
