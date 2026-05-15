b3a0310c2ef9c9aa3bb835dcce5e4ecec2e12bd0
AbuJubair7 <jubaircode7@gmail.com>
2026-05-15 11:50:45 +0600

    fix(experienceService): async await added for the cruid operation

--- src/experiences/experiences.service.ts
+++ src/experiences/experiences.service.ts
@@ -12,8 +12,8 @@ export class ExperiencesService {
     @InjectModel(Experience.name) private experienceModel: Model<Experience>,
   ) {}
 
-  create(createExperienceDto: CreateExperienceDto, id: string) {
-    const exp = this.experienceModel.create({
+  async create(createExperienceDto: CreateExperienceDto, id: string) {
+    const exp = await this.experienceModel.create({
       _id: crypto.randomUUID(),
       ...createExperienceDto,
       userId: id,
@@ -21,25 +21,25 @@ export class ExperiencesService {
     return exp;
   }
 
-  findAll(id: string) {
-    const experiences = this.experienceModel.find({ userId: id }).exec();
+  async findAll(id: string) {
+    const experiences = await this.experienceModel.find({ userId: id }).exec();
     return experiences;
   }
 
-  findOne(id: string) {
-    const exp = this.experienceModel.findById(id).exec();
+  async findOne(id: string) {
+    const exp = await this.experienceModel.findById(id).exec();
     return exp;
   }
 
-  update(id: string, updateExperienceDto: UpdateExperienceDto) {
-    const exp = this.experienceModel
+  async update(id: string, updateExperienceDto: UpdateExperienceDto) {
+    const exp = await this.experienceModel
       .findByIdAndUpdate(id, updateExperienceDto, { new: true })
       .exec();
     return exp;
   }
 
-  remove(id: string) {
-    const exp = this.experienceModel.findByIdAndDelete(id).exec();
+  async remove(id: string) {
+    const exp = await this.experienceModel.findByIdAndDelete(id).exec();
     return { message: 'Experience deleted successfully', experience: exp };
   }
 }
