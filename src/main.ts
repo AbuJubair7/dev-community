8b63add399e0a495c86b3b8058f056fcb49deddc
AbuJubair7 <jubaircode7@gmail.com>
2026-05-18 12:46:17 +0600

    fix: Enable cros from different origin

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,18 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // await connectDB();
+
+  app.enableCors({
+    origin: [
+      'http://localhost:5173', // Vite dev server
+      'http://localhost:4173', // Vite preview
+      'http://127.0.0.1:5173',
+    ],
+    methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
