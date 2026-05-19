b57b5853e8e487104f716bf7a1a2c28c8c2d8a89
AbuJubair7 <jubaircode7@gmail.com>
2026-05-19 08:45:24 +0600

    feat(commnets): Comments schema added

--- src/modules/comments/entities/comment.entity.ts
+++ src/modules/comments/entities/comment.entity.ts
@@ -1,1 +1,21 @@
-export class Comment {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type CommentDocument = HydratedDocument<Comment>;
+
+@Schema({ timestamps: true })
+export class Comment {
+  @Prop({ type: String, primary: true })
+  _id!: string;
+
+  @Prop()
+  postId!: string;
+
+  @Prop()
+  userId!: string;
+
+  @Prop({ required: true })
+  content!: string;
+}
+
+export const CommentSchema = SchemaFactory.createForClass(Comment);
